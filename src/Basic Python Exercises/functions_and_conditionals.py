d9469abc83f2701b41f4f090db8be5fa04277764
Samueljbk <samjbkyle@gmail.com>
2023-05-23 19:50:48 +1200

    added completed functions and conditionals exercise

--- src/Basic Python Exercises/functions_and_conditionals.py
+++ src/Basic Python Exercises/functions_and_conditionals.py
@@ -2,3 +2,16 @@
 # and calculates the number of upper case letters and lower case letters.
 
 string = input("Enter a string: ")
+
+uppercase = 0
+lowercase = 0
+
+for char in string:
+    if char.isupper() == True:
+        uppercase += 1
+    if char.islower() == True:
+        lowercase += 1
+
+print(
+    f"There are {uppercase} uppercase characters and {lowercase} lowercase characters"
+)
