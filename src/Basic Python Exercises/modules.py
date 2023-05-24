3e74f0c0f1c9e1f84f89ea1040e9412500b4b748
Samueljbk <samjbkyle@gmail.com>
2023-05-25 10:49:48 +1200

    added basic math module exercise, function to get square root of number

---
+++ src/Basic Python Exercises/modules.py
@@ -0,0 +1,11 @@
+# Use the Python math module to calculate the square root of a number.
+
+import math
+
+
+def get_square_root():
+    number = int(input("Enter a number to find the square root of: "))
+    print(math.sqrt(number))
+
+
+get_square_root()
