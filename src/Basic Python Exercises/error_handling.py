5c47ac60306e1c3b0b9faef3fa34a025b2250dec
Samueljbk <samjbkyle@gmail.com>
2023-05-25 09:23:41 +1200

    Fixed exercise

--- src/Basic Python Exercises/error_handling.py
+++ src/Basic Python Exercises/error_handling.py
@@ -3,9 +3,12 @@
 # the program should print a message and ask for the input again.
 # Keep asking until the user inputs a number.
 
-user_input = input("Enter a number: ")
+correct = 0
 
-if user_input.isdigit():
-    print(user_input)
-else:
-    print("That's not a number - please enter an integer number")
+while correct == 0:
+    user_input = input("Enter a number: ")
+    if user_input.isdigit():
+        print(f"The number you entered was: {user_input}")
+        correct = 1
+    else:
+        print("That's not a number - please enter an integer number")
