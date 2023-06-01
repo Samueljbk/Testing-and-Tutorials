410333b368e4a7ba5e5630eb9ed8350e388bb0ba
Samueljbk <samjbkyle@gmail.com>
2023-06-01 19:33:17 +1200

    Added classes and objects folder and started some tutorials

---
+++ src/Basic Python Exercises/Classes and Objects/dog.py
@@ -0,0 +1,11 @@
+class Dog:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def bark(self):
+        print("Woof, woof!")
+
+
+my_dog = Dog("Gemma", 15)
+my_dog.bark()
