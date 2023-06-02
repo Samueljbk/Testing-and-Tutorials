e33982c5d8bb0535d2938675457eaf3dc71c08f6
Samueljbk <samjbkyle@gmail.com>
2023-06-02 23:47:42 +1200

    added numbers to classes and objects

---
+++ src/Basic Python Exercises/Classes and Objects/02.circle.py
@@ -0,0 +1,11 @@
+# Create a class called Circle with an instance variable radius. Add an __init__ method to initialize the radius.
+# Add two methods: area and circumference that return the area and circumference of the circle, respectively.
+# Use the constant pi from the math module.
+from math import pi
+
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        
