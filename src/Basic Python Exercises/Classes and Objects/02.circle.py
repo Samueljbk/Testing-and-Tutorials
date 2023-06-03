5e428d60d666f61333c63f5f77d09edfe764b2c0
Samueljbk <samjbkyle@gmail.com>
2023-06-03 22:13:43 +1200

    added circle class exercise

--- src/Basic Python Exercises/Classes and Objects/02.circle.py
+++ src/Basic Python Exercises/Classes and Objects/02.circle.py
@@ -1,11 +1,15 @@
 # Create a class called Circle with an instance variable radius. Add an __init__ method to initialize the radius.
 # Add two methods: area and circumference that return the area and circumference of the circle, respectively.
 # Use the constant pi from the math module.
-from math import pi
+import math
+
 
 class Circle:
     def __init__(self, radius):
         self.radius = radius
 
     def area(self):
-        
+        return math.pi * self.radius**2
+
+    def circumference(self):
+        return 2 * math.pi * self.radius
