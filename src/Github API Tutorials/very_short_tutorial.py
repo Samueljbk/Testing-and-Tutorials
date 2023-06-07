2277b1567db1ac53c743039ec20d28d84791d674
Samueljbk <samjbkyle@gmail.com>
2023-06-07 23:24:55 +1200

    Added time check for latest pushevent

--- src/Github API Tutorials/very_short_tutorial.py
+++ src/Github API Tutorials/very_short_tutorial.py
@@ -1,18 +1,21 @@
 from github import Github
 import os
 import datetime
+import time
 
 token = os.environ["PERSONAL_ACCESS_TOKEN"]
 
 g = Github(token)
-user = g.get_user("Samueljbk")
-all_events = list(user.get_events())
-push_events = [e for e in all_events if e.type == "PushEvent"]
-latest_event = max(push_events, key=lambda e: e.created_at)
-latest_event_time = latest_event.created_at
+while True:
+    user = g.get_user("Samueljbk")
+    all_events = list(user.get_events())
+    push_events = [e for e in all_events if e.type == "PushEvent"]
+    latest_event = max(push_events, key=lambda e: e.created_at)
+    latest_event_time = latest_event.created_at
 
-if latest_event_time.date() == datetime.date.today():
-    print("Pushed Today")
+    if latest_event_time.date() == datetime.date.today():
+        print("Pushed Today")
 
-print(latest_event)
+    time.sleep(0)
+
 print(latest_event)
