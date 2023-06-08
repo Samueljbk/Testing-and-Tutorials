0a9f823f79d39bb2d57afb9a0bf3c4e3c3f5a995
Samueljbk <samjbkyle@gmail.com>
2023-06-08 23:36:31 +1200

    added numbers to various files

---
+++ src/Github API Tutorials/02.very_short_tutorial.py
@@ -0,0 +1,19 @@
+from github import Github
+import os
+import datetime
+import time
+
+token = os.environ["PERSONAL_ACCESS_TOKEN"]
+
+g = Github(token)
+
+user = g.get_user("Samueljbk")
+all_events = list(user.get_events())
+push_events = [e for e in all_events if e.type == "PushEvent"]
+latest_event = max(push_events, key=lambda e: e.created_at)
+latest_event_time = latest_event.created_at
+if latest_event_time.date() == datetime.date.today():
+    print("Pushed Today")
+
+print(latest_event)
+print(latest_event)
