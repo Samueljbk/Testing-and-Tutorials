d1004da6a2e156bac77e24753e45f4f930a10499
Samueljbk <samjbkyle@gmail.com>
2023-06-13 23:00:56 +1200

    Starting python exercise roadmap

---
+++ src/Python Exercises/Basic Syntax/02.varibles_and_data_types.py
@@ -0,0 +1,10 @@
+# Create three variables: an integer named age, a float named height, and a string named name.
+# Assign appropriate values to these variables and print them.
+
+age = 27
+
+height = 194
+
+name = "Sam"
+
+print(f"{name} is {height}cm tall and {age} years old")
