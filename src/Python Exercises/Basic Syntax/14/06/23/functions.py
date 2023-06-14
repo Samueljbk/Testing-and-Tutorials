a502074f6ac4ae65f660120b73979f873968fa89
Samueljbk <samjbkyle@gmail.com>
2023-06-14 19:20:20 +1200

    adding basic exercises

---
+++ src/Python Exercises/Basic Syntax/14/06/23/functions.py
@@ -0,0 +1,10 @@
+# Create a function called area_of_rectangle that takes two arguments: width and length.
+# The function should return the area of a rectangle (width * length).
+# Call this function with different values for width and length and print the resulting area.
+
+
+def area_of_rectangle(width: int, length: int):
+    return width * length
+
+
+print(area_of_rectangle(10, 5))
---
+++ src/Python Exercises/Basic Syntax/14/06/23/user_input.py
@@ -0,0 +1,7 @@
+# Write a Python program that asks the user to input their name and age.
+# Then, print a message that says, "Hello, [name]! You are [age] years old."
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+print(f"Hello, {name}! You are {age} years old")
