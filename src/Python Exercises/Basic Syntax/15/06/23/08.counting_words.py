b797f3b5ef0552b357cba8842ff794a984093be8
Samueljbk <samjbkyle@gmail.com>
2023-06-16 22:49:10 +1200

    counting_words

--- src/Python Exercises/Basic Syntax/15/06/23/08.counting_words.py
+++ src/Python Exercises/Basic Syntax/15/06/23/08.counting_words.py
@@ -1,2 +1,14 @@
 # Create a program that takes a string as user input and counts the number of words in the string.
 # Print the word count.
+
+
+def count_words(input_string):
+    words = input_string.split()
+    word_count = len(words)
+    return word_count
+
+
+user_input = input("Enter a string: ")
+word_count = count_words(user_input)
+
+print("Word count:", word_count)
